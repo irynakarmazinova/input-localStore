406ce57fc466dd85f79100b56588da100c6ee63c
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-07-23 11:08:11 +0300

    добавила копию

--- src/index.js
+++ src/index.js
@@ -16,28 +16,21 @@ const formData = {};
  * - 3. Очищаем форму после отправки сообщения (oчищаем localStorage)
  */
 function onFormInput(e) {
-  // имитация отправки формы
   e.preventDefault(); //1.
-
-  // console.log('Отправляем форму');
-
-  // e.currentTarget - это сейчас форма, потому что onFormInput сейчас висит на formRef
-  // нельзя писать this.reset() - в этих методах нельзя надеется на контекст
   e.currentTarget.reset(); //2.
-
   localStorage.removeItem(STORAGE_KEY); //3.
 }
 formRef.addEventListener('submit', onFormInput);
 
 // когда сложные формы и много полей
 // сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
-function onFormInputs(e) {
-  formData[e.target.name] = e.target.value;
-  formData[e.target.message] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  console.log(formData);
-}
-formRef.addEventListener('input', onFormInputs);
+// function onFormInputs(e) {
+//   formData[e.target.name] = e.target.value;
+//   formData[e.target.message] = e.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+//   console.log(formData);
+// }
+// formRef.addEventListener('input', onFormInputs);
 
 /*
  * - 1. Получаем значение(данные) поля textarea при вводе
@@ -46,13 +39,7 @@ formRef.addEventListener('input', onFormInputs);
  */
 function onTextareaInput(e) {
   const message = e.target.value; //1.
-
-  // const message = e.currentTarget.value; //из-за всплытия буду ошибки, не использовать
-  // console.log(message);
-
   localStorage.setItem(STORAGE_KEY, message); //2.
-  //value текстареи - это строка.
-  // т.е. стрингифаить (JSON.stringify(message)) не нужно, стрингифаят только массивы и объекты
 }
 textareaRef.addEventListener('input', throttle(onTextareaInput, 500));
 
@@ -65,11 +52,80 @@ textareaRef.addEventListener('input', throttle(onTextareaInput, 500));
  */
 function populateTextarea() {
   const savedMessage = localStorage.getItem(STORAGE_KEY); //1.
-
   // 2.
   if (savedMessage) {
-    // console.log(savedMessage);
     textareaRef.value = savedMessage; //3.
   }
 }
 populateTextarea();
+
+// --------------------------------------------------------------------------
+// полная копия
+// константа - константное значение между разными запусками скрипта
+// const STORAGE_KEY = 'feedback-message';
+
+// const formData = {};
+
+// /*
+//  * - 1. Остановить(запретить) поведение по умолчанию
+//  * - 2. Убираем сообщение из хранилища (метод reset - сбрасывает значение всех полей в начальное значение)
+//  * - 3. Очищаем форму после отправки сообщения (oчищаем localStorage)
+//  */
+// function onFormInput(e) {
+//   // имитация отправки формы
+//   e.preventDefault(); //1.
+
+//   // console.log('Отправляем форму');
+
+//   // e.currentTarget - это сейчас форма, потому что onFormInput сейчас висит на formRef
+//   // нельзя писать this.reset() - в этих методах нельзя надеется на контекст
+//   e.currentTarget.reset(); //2.
+
+//   localStorage.removeItem(STORAGE_KEY); //3.
+// }
+// formRef.addEventListener('submit', onFormInput);
+
+// // когда сложные формы и много полей
+// // сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
+// // function onFormInputs(e) {
+// //   formData[e.target.name] = e.target.value;
+// //   formData[e.target.message] = e.target.value;
+// //   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// //   console.log(formData);
+// // }
+// // formRef.addEventListener('input', onFormInputs);
+
+// /*
+//  * - 1. Получаем значение(данные) поля textarea при вводе
+//  * - 2. Сохраняем его в хранилище
+//  * - 3. Можно добавить throttle
+//  */
+// function onTextareaInput(e) {
+//   const message = e.target.value; //1.
+
+//   // const message = e.currentTarget.value; //из-за всплытия буду ошибки, не использовать
+//   // console.log(message);
+
+//   localStorage.setItem(STORAGE_KEY, message); //2.
+//   //value текстареи - это строка.
+//   // т.е. стрингифаить (JSON.stringify(message)) не нужно, стрингифаят только массивы и объекты
+// }
+// textareaRef.addEventListener('input', throttle(onTextareaInput, 500));
+
+// /*
+// //
+//  * populateTextarea будет вызываться(выполняться) при загрузке страницы
+//  * - 1. Получаем значение из хранилища
+//  * - 2. Всегда нужно проверять есть ли что-то в поле ввода. если да, то выполняем код
+//  * - 3. Если там что-то было, обновляем DOM
+//  */
+// function populateTextarea() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY); //1.
+
+//   // 2.
+//   if (savedMessage) {
+//     // console.log(savedMessage);
+//     textareaRef.value = savedMessage; //3.
+//   }
+// }
+// populateTextarea();
