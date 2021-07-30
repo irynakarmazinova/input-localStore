044596e26df41ce776ca5f08bec693f5502bd929
irynakarmazinova <i.karmazinova1992@gmail.com>
2021-07-30 20:16:38 +0300

    add new comments and function throttle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
+// import throttle from 'lodash.throttle';
+
 // импорты js положено ставить выше css
-import throttle from 'lodash.throttle';
-import './style.css';
+// если это обычный css (не sass, module и т.д.), то его подключают к js только на время разработки, а так только к html. js не понимает css и если нет парсера любого(webpack или parcel), то такой код упадет.
+
+// константа - константное значение между разными запусками скрипта
+// глобальная константа через Upper Case + Kebab Case должна быть или в самом верху, сразу после импортов или вообще в другом файле типа constants.js и в импорте извлекаться(import { STORAGE_KEY } from './constants,js';)
+const STORAGE_KEY = 'feedback-message';
 
 const formRef = document.querySelector('.js-feedback-form'); //вся форма
 const textareaRef = document.querySelector('.js-feedback-form textarea'); //только текстареа
 const nameInputRef = document.querySelector('.js-feedback-form input[name="name"]'); //только инпут для ввода имени
 const checkboxRef = document.getElementById('user-choise'); //только чекбокс
-
-// константа - константное значение между разными запусками скрипта
-const STORAGE_KEY = 'feedback-message';
 
 const formData = {};
 
@@ -41,22 +43,58 @@ formRef.addEventListener('input', throttle(onAllFormsInput, 500));
  * - 3. Если там что-то было, обновляем DOM
  */
 function populateTextarea() {
-  let savedMessage = localStorage.getItem(STORAGE_KEY); //1.
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)); //1.
 
   // 2.
   if (savedMessage) {
-    savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
     nameInputRef.value = savedMessage.name; //3.
     textareaRef.value = savedMessage.message;
 
     // если у savedMessage есть agreement(тоесть чекбокс выбран) - то выбрать его, а если нет, то не выбирать
-    savedMessage.agreement ? (checkboxRef.checked = true) : (checkboxRef.checked = false);
-    // checkboxRef.checked = Boolean(checkboxRef);
+    // не правильно делать тринарник без присваивания или без return, здесь правильно
+    // savedMessage.agreement ? (checkboxRef.checked = true) : (checkboxRef.checked = false);
+
+    checkboxRef.checked = savedMessage.agreement ? true : false;
+
+    // checkboxRef.checked = !!savedMessage.agreement;
+    // checkboxRef.checked = Boolean(savedMessage.agreement);
+
+    // !!savedMessage.agreement === Boolean(savedMessage.agreement);
+
+    // Двойной восклицательный знак - позволяет конвертировать любое выражение в логическое значение. Если выражение, с точки зрения JS, истинно — после обработки его двойным восклицательным знаком будет возвращено true. В противном случае будет возвращено false.
   }
 }
 populateTextarea();
 
+// что бы не подключать весь lodash, можно использовать функцию throttle
+function throttle(func, ms) {
+  let isThrottled = false;
+  let savedArgs;
+  let savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
 // --------------------------------------------------------------------------
 // простой вариант - без использвания объекта
 
